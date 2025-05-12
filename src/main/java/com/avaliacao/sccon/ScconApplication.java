2a5d60cd796a3facc9e79bba9f241fac30fc0ac6
Emerson Costa <emersonjscosta@gmail.com>
2025-05-12 11:06:48 -0300

    Iniciando o projeto.

---
+++ src/main/java/com/avaliacao/sccon/ScconApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.sccon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScconApplication.class, args);
+	}
+
+}
